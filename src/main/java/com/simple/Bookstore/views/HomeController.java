14ead7b749ef168d20f074dd0e761e5b2f438607
am-cid <mcidantonio@gmail.com>
2025-08-22 01:20:09 +0800

    feat(templates): add slogan, site logo, and nav bar (all svg)
    randomized some tilt and y offset for site logo letters. man svg is a
    pain in the

--- src/main/java/com/simple/Bookstore/views/HomeController.java
+++ src/main/java/com/simple/Bookstore/views/HomeController.java
@@ -17,8 +17,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Slf4j
 @Controller
@@ -50,6 +49,12 @@ public class HomeController {
         model.addAttribute("_overrideTheme", themeUsedAsInlineCss);
         model.addAttribute("_overrideThemeUsed", themeUsed);
 
+        // header data
+        model.addAttribute(
+                "logoLetters",
+                getLogoLettersModelAttributes("BOOKSTORE")
+        );
+
         // center content data
         List<BookSearchResultDTO> latestBooks = bookService.findLatestNBooks(10); // Find more than needed to fill all sections
         model.addAttribute("bannerBooks", latestBooks.subList(0, Math.min(6, latestBooks.size())));
@@ -79,5 +84,47 @@ public class HomeController {
         model.addAttribute("top5Books", bookService.findTopNRatedBooks(5)); // A new service method
 
         return "index";
+    }
+
+    private List<Map<String, Object>> getLogoLettersModelAttributes(String siteName) {
+        char[] letters = siteName.toCharArray();
+        List<Map<String, Object>> attributes = new ArrayList<>();
+        for (int i = 0; i < letters.length; i++) {
+            Map<String, Object> attribute = new HashMap<>();
+            attribute.put("char", String.valueOf(letters[i]));
+            attribute.put("id", "logo-letter-" + i);
+            int rotate = switch (i) {
+                case 1,
+                     7 ->
+                        -10;
+                case 2 ->
+                        10;
+                case 4 ->
+                        5;
+                case 5 ->
+                        -5;
+                default ->
+                        0;
+            };
+            attribute.put("rotate", rotate);
+            int yOffset = switch (i) {
+                case 1,
+                     6 ->
+                        2;
+                case 2,
+                     7 ->
+                        -2;
+                case 3 ->
+                        1;
+                case 5 ->
+                        -3;
+                default ->
+                        0;
+            };
+            attribute.put("y", yOffset);
+            attribute.put("x", i * 38);
+            attributes.add(attribute);
+        }
+        return attributes;
     }
 }
