187bc46fac48e8a188e68505357937ba37ce99a8
am-cid <mcidantonio@gmail.com>
2025-09-08 23:35:38 +0800

    feat(views): initial book page view

---
+++ src/main/java/com/simple/Bookstore/views/BookViewController.java
@@ -0,0 +1,45 @@
+package com.simple.Bookstore.views;
+
+import com.simple.Bookstore.Book.BookSearchResultDTO;
+import com.simple.Bookstore.Book.BookService;
+import com.simple.Bookstore.Review.ReviewResponseDTO;
+import com.simple.Bookstore.Review.ReviewService;
+import com.simple.Bookstore.Theme.ThemeService;
+import com.simple.Bookstore.User.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
+public class BookViewController {
+    private final BookService bookService;
+    private final ReviewService reviewService;
+    private final ThemeService themeService;
+
+    @GetMapping("/books/{bookId}")
+    public String getBook(
+            @PathVariable Long bookId,
+            @AuthenticationPrincipal User user,
+            @PageableDefault Pageable pageable,
+            Model model
+    ) {
+        HeaderAndSidebarsModelAttributes.defaults(user, model, bookService, reviewService, themeService);
+        Optional<BookSearchResultDTO> bookResult = bookService.findBookById(bookId);
+        if (bookResult.isEmpty())
+            // TODO: redirect to unknown book page
+            return "redirect:/";
+        Page<ReviewResponseDTO> reviews = reviewService.findAllReviewsByBookIdAsPage(bookId, pageable);
+        model.addAttribute("book", bookResult.get());
+        model.addAttribute("review", reviews);
+        return "book";
+    }
+}
