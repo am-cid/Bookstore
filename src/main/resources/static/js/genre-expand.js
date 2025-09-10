0492ad016ecb6249c2bd2234442f72965710a880
am-cid <mcidantonio@gmail.com>
2025-09-10 18:57:36 +0800

    refactor(templates): make genres list into its own fragment
    still need to link the script outside the fragment. TODO: find a way to
    link the script inside the fragment, only loading the function once. not
    high priority though

--- src/main/resources/static/js/genre-expand.js
+++ src/main/resources/static/js/genre-expand.js
@@ -1,29 +1,27 @@
-(function () {
-    document.querySelectorAll(".list-item-details ul").forEach(ul => {
-        const items = Array.from(ul.querySelectorAll("li"));
-        const cutoff = ul.dataset.cutoff == null
-            ? 6
-            : parseInt(ul.dataset.cutoff, 10);
-        if (items.length > cutoff) {
-            const toggleLi = document.createElement("li");
-            toggleLi.textContent = "More...";
-            toggleLi.classList.add("toggle-genre");
-            toggleLi.classList.add("more-less-link");
-            toggleLi.classList.add("genre-oval");
-            toggleLi.classList.add("genre-color-style-00");
-            ul.appendChild(toggleLi);
-            toggleLi.addEventListener("click", () => {
-                const isExpanded = toggleLi.textContent === "Less...";
-                if (isExpanded) {
-                    // collapse
-                    items.slice(cutoff).forEach(li => li.classList.add("hidden-genre"));
-                    toggleLi.textContent = "More...";
-                } else {
-                    // expand
-                    items.slice(cutoff).forEach(li => li.classList.remove("hidden-genre"));
-                    toggleLi.textContent = "Less...";
-                }
-            });
-        }
-    });
-})();
+document.querySelectorAll(".genres-ul").forEach(ul => {
+    const items = Array.from(ul.querySelectorAll("li"));
+    const cutoff = ul.dataset.cutoff == null
+        ? 6
+        : parseInt(ul.dataset.cutoff, 10);
+    if (items.length > cutoff) {
+        const toggleLi = document.createElement("li");
+        toggleLi.textContent = "More...";
+        toggleLi.classList.add("toggle-genre");
+        toggleLi.classList.add("more-less-link");
+        toggleLi.classList.add("genre-oval");
+        toggleLi.classList.add("genre-color-style-00");
+        ul.appendChild(toggleLi);
+        toggleLi.addEventListener("click", () => {
+            const isExpanded = toggleLi.textContent === "Less...";
+            if (isExpanded) {
+                // collapse
+                items.slice(cutoff).forEach(li => li.classList.add("hidden-genre"));
+                toggleLi.textContent = "More...";
+            } else {
+                // expand
+                items.slice(cutoff).forEach(li => li.classList.remove("hidden-genre"));
+                toggleLi.textContent = "Less...";
+            }
+        });
+    }
+});
