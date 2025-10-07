65f04da408536212591e0bdb469ac6555c751f7b
am-cid <mcidantonio@gmail.com>
2025-10-07 23:18:02 +0800

    feat(views): initial create theme view
    feat(Theme): add default theme as as static method (to be used by
    thymeleaf as fallback if override is not present when previewing in
    create theme form).
    
    feat(Theme): add validation for theme request DTO

--- src/main/java/com/simple/Bookstore/Theme/ThemeRequestDTO.java
+++ src/main/java/com/simple/Bookstore/Theme/ThemeRequestDTO.java
@@ -1,16 +1,59 @@
 package com.simple.Bookstore.Theme;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record ThemeRequestDTO(
+        @NotBlank(message = "Theme must have a name")
+        @Size(min = 3, max = 20, message = "Theme name must be between 3 and 100 characters")
         String name,
+
+        @Size(max = 2000, message = "Stop yapping! 2000 chars only!")
         String description,
-        boolean published,
+
+        Boolean published,
+
+        @NotBlank(message = "Theme must have color 0")
         String base00,
+
+        @NotBlank(message = "Theme must have color 1")
         String base01,
+
+        @NotBlank(message = "Theme must have color 2")
         String base02,
+
+        @NotBlank(message = "Theme must have color 3")
         String base03,
+
+        @NotBlank(message = "Theme must have color 4")
         String base04,
+
+        @NotBlank(message = "Theme must have color 5")
         String base05,
+
+        @NotBlank(message = "Theme must have color 6")
         String base06,
+
+        @NotBlank(message = "Theme must have color 7")
         String base07
 ) {
+    public static ThemeRequestDTO empty() {
+        return new ThemeRequestDTO(
+                null, null, false,
+                null, null, null, null, null, null, null, null
+        );
+    }
+
+    public boolean isEmpty() {
+        return name == null
+                && description == null
+                && base00 == null
+                && base01 == null
+                && base02 == null
+                && base03 == null
+                && base04 == null
+                && base05 == null
+                && base06 == null
+                && base07 == null;
+    }
 }
