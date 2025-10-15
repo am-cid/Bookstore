22920deeb6587dff857a11978e536205101bbc88
nofa <noor.mustafa@soco-engineers.com>
2025-10-15 21:45:16 +0500

    delete button books page changes

--- src/main/java/com/simple/Bookstore/views/BookViewController.java
+++ src/main/java/com/simple/Bookstore/views/BookViewController.java
@@ -1,9 +1,11 @@
 package com.simple.Bookstore.views;
 
+import com.simple.Bookstore.Auth.RegisterRequestDTO;
 import com.simple.Bookstore.Book.BookSearchResultDTO;
 import com.simple.Bookstore.Book.BookService;
 import com.simple.Bookstore.Exceptions.BookNotFoundException;
 import com.simple.Bookstore.Exceptions.ReviewNotFoundException;
+import com.simple.Bookstore.Profile.ProfileEditRequestDTO;
 import com.simple.Bookstore.Profile.ProfileService;
 import com.simple.Bookstore.Review.ReviewBookViewResponseDTO;
 import com.simple.Bookstore.Review.ReviewRequestDTO;
@@ -11,6 +13,9 @@ import com.simple.Bookstore.Review.ReviewResponseDTO;
 import com.simple.Bookstore.Review.ReviewService;
 import com.simple.Bookstore.Theme.ThemeService;
 import com.simple.Bookstore.User.User;
+import com.simple.Bookstore.utils.Result;
+import com.simple.Bookstore.views.Profile.ProfileEditModel;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,6 +26,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,6 +39,7 @@ public class BookViewController {
     private final ReviewService reviewService;
     private final ThemeService themeService;
     private final ProfileService profileService;
+
 
     @GetMapping("/books/{bookId}")
     public String getBook(
@@ -72,13 +80,15 @@ public class BookViewController {
         return "book";
     }
 
-    //    http://localhost:8080/books/2/reviews/24/delete
-    @GetMapping("/books/{bookId}/reviews/{reviewId}/delete")
-    public String deleteReview(Model model, @PathVariable("bookId") Long bookId, @PathVariable("reviewId") Long reviewId, @ModelAttribute("reviewRequestDTO") ReviewRequestDTO reviewRequest,
+    // returns the confirmation page
+    @GetMapping("/books/{bookId}/reviews/{reviewId}/delete/confirm")
+    public String confirmEditProfile(
+            @PathVariable("bookId") Long bookId,
+            @PathVariable("reviewId") Long reviewId,
             @AuthenticationPrincipal User user,
-            @PageableDefault Pageable pageable ) {
-        System.out.println("method executed");
-        HeaderAndSidebarsModelAttributes.defaults( user,model,bookService,reviewService,themeService );
+            Model model
+    ) {
+
         // null checks
         Optional<BookSearchResultDTO> bookResult = bookService.findBookById(bookId);
 
@@ -89,32 +99,45 @@ public class BookViewController {
         if( existingReviewDTO == null ){
             throw new ReviewNotFoundException(reviewId);
         }
+
+        HeaderAndSidebarsModelAttributes.defaults(user, model, bookService, reviewService, themeService);
+        // need properties to persist till the result call
+        model.addAttribute("bookId", bookId);
+        model.addAttribute("reviewId", reviewId);
+
+        //! need to create the review delete confirm page
+        return "review-delete-confirm-page";
+    }
+
+    //  review delete confirmation button call
+    @PostMapping("/books/{bookId}/reviews/{reviewId}/delete/confirm")
+    public String confirmEditProfile(
+            @PathVariable("bookId") Long bookId,
+            @PathVariable("reviewId") Long reviewId
+    ) {
+        return String.format("redirect:/books/%d/reviews/%d/delete/result", bookId, reviewId);
+    }
+
+    // review delete back button call
+    @PostMapping(path = "/books/{bookId}/reviews/{reviewId}/delete/confirm", params = {"_action=back"})
+    public String confirmEditProfileGoBack(
+            @PathVariable("bookId") Long bookId,
+            @PathVariable("reviewId") Long reviewId
+    ) {
+        return String.format("redirect:/books/%d", bookId);
+    }
+
+    @GetMapping("/books/{bookId}/reviews/{reviewId}/delete/result")
+    public String deleteReview(Model model, @PathVariable("bookId") Long bookId, @PathVariable("reviewId") Long reviewId, @ModelAttribute("reviewRequestDTO") ReviewRequestDTO reviewRequest,
+            @AuthenticationPrincipal User user
+    ) {
+        HeaderAndSidebarsModelAttributes.defaults( user,model,bookService,reviewService,themeService );
+
         // deleting review
         reviewService.deleteReview( user, reviewId );
-        // getting all remaining reviews
-        Page<ReviewBookViewResponseDTO> reviews = reviewService.findAllPublicOrOwnedReviewsByBookIdAsPage(
-                bookId,
-                user != null ? user.getProfile().getId() : null,
-                pageable
-        );
-        Long reviewCount = reviewService.countByBookId(bookId);
-        List<Long> savedBookIds = bookService.findSavedBooks(user)
-                .stream()
-                .map(BookSearchResultDTO::id)
-                .toList();
-        boolean alreadyReviewed = reviewService.isAlreadyReviewedByUser(bookId, user);
+        HeaderAndSidebarsModelAttributes.defaults(user, model, bookService, reviewService, themeService);
 
-        model.addAttribute("book", bookResult.get());
-        model.addAttribute("reviews", reviews);
-        model.addAttribute("reviewCount", reviewCount);
-        model.addAttribute("savedBookIds", savedBookIds);
-        model.addAttribute("alreadyReviewed", alreadyReviewed);
-        model.addAttribute(
-                "reviewRequestDTO",
-                reviewRequest.isEmpty()
-                        ? ReviewRequestDTO.empty()
-                        : reviewRequest
-        );
-        return "book";
+        //! need to create the review delete result page
+        return "review-delete-result";
     }
 }
